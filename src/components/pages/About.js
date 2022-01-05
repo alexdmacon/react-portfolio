c909cf6aaedd58a5fd220992c683d84009e70ddb
alexdmacon <alexdmacon@gmail.com>
2022-01-04 20:29:43 -0600

    fiddled with weird flexing on about page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,23 +1,22 @@
 import React from "react";
 import headshot from "../../assets/images/new-me.jpeg"
 
+
 const About = () => {
   return (
-    <div className="row d-flex justify-content-center align-items-center">
-      <div class="col-4">
+    <div className="d-flex flex-column flex-md-row justify-content-between mb-5">
+      <div className="flex-shrink-0 align-items-center">
         <img className="headshot" src={headshot} alt="headshot"></img>
       </div>
 
-      <div class="col-8">
+      <div className="flex-grow-1">
         <p>
           After years as a professional journalist writing about the day’s
           biggest stories, I decided that I wanted to learn to write code.
           Because what’s a bigger story than tech?
         </p>
         <p>
-          Now I am a full-stack developer seeking full-time employment in the
-          field. This is less of a career change than the logical next step for
-          me. I’ve always sought out challenges. Curiosity, self-reliance, and
+        Curiosity, self-reliance, and
           grit are fundamental to both journalism and programming. I have the
           tenacity and drive to figure out new problems, and I push myself to
           learn more every day. I have a positive, upbeat attitude and embrace
