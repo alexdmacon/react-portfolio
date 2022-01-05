13851742af8f28320d8de3953fa05c8dcc4b3a20
alexdmacon <alexdmacon@gmail.com>
2022-01-04 22:13:16 -0600

    Contact form set up with name and email validation. No backend so no submit obv. Will style everything later.

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,32 +1,85 @@
-import React from "react";
+import React, { useState } from "react";
 import validateEmail from "../../utils/validateEmail";
 
-function Form() {
-    
-}
-
-
-
 const Contact = () => {
+  const [formState, setFormState] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const { name, email, message } = formState;
+
+  function handleChange(e) {
+    if (e.target.name === "email") {
+      const isValid = validateEmail(e.target.value);
+
+      if (!isValid) {
+        setErrorMessage("You must enter a valid email");
+      } else {
+        setErrorMessage("");
+      }
+    } else {
+      if (!e.target.value.length) {
+        setErrorMessage(`${e.target.name} is a required entry.`);
+      } else {
+        setErrorMessage("");
+      }
+    }
+
+    if (!errorMessage) {
+      setFormState({ ...formState, [e.target.name]: e.target.value });
+    }
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
   return (
     <div className="container">
       <h2>Contact Me</h2>
-      <form>
+      <form id="form-contact" onSubmit={handleSubmit}>
         <label for="name">Name</label>
-        <input type="text"></input>
+        <input
+          type="text"
+          name="name"
+          defaultValue={name}
+          onBlur={handleChange}
+        ></input>
         <label for="email"> Email </label>
-        <input type="text"></input>
+        <input
+          type="email"
+          name="email"
+          defaultValue={email}
+          onBlur={handleChange}
+        ></input>
         <label for="name">Message</label>
-        <textarea></textarea>
+        <textarea
+          name="message"
+          rows="3"
+          defaultValue={message}
+          onBlue={handleChange}
+        ></textarea>
         <button type="submit">Submit</button>
+
+{/* if user doesn't enter name or email correctly (meaning the handleChange function finds something false) then display error message) */}
+        <div>
+          {errorMessage && (
+            <div>
+              <h3>{errorMessage}</h3>
+            </div>
+          )}
+        </div>
       </form>
 
-
-
-
-
-    <h2>Better Yet...</h2>
-    <p>Just email me: <a href="mailto:alexdmacon@gmail.com">alexdmacon@gmail.com</a></p>
+      <h2>Better Yet...</h2>
+      <p>
+        Just email me:{" "}
+        <a href="mailto:alexdmacon@gmail.com">alexdmacon@gmail.com</a>
+      </p>
     </div>
   );
 };
